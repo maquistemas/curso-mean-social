06c0b576ed52a58873ddf2e37659a0fc73194a70
john <maquistemas@gmail.com>
2018-06-12 15:10:59 -0500

    Subir avatar de usuario I

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,6 +3,8 @@
 //bcrypt para cifrar las contraseñas
 var bcrypt = require('bcrypt-nodejs');
 var mongoosePaginate = require('mongoose-pagination');
+var fs = require('fs');//permite trabajar con archivos
+var path = require('path');//permite trabajarcon rutas de ficheros
 
 var User = require('../models/user');
 var jwt = require('../services/jwt');
@@ -188,6 +190,54 @@ function updateUser(req, res){
 }
 
 
+//Subir archivos de imagen/avatar de usuario
+function uploadImage(req, res){
+		var userId = req.params.id;
+		
+		
+		if(req.files){
+			var file_path = req.files.image.path;
+			console.log(file_path);
+			var file_split = file_path.split('\\');
+			console.log(file_split);
+
+			var file_name = file_split[2];
+			console.log(file_name);
+
+			var ext_split = file_name.split('\.');
+			console.log(ext_split);
+			var file_ext = ext_split[1];
+			console.log(file_ext);
+
+
+			if(userId != req.user.sub){
+			   removeFilesOfUploads(res, file_path, 'No tienes permiso para actualizar los datos del usuario');
+			}
+
+
+			if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif'){
+				//Actualizar documento de usuario logueado
+
+
+			}else{
+				removeFilesOfUploads(res, file_path, 'Extensión no válida');
+
+			}
+
+		}else{
+			return res.status(200).send({message: 'No se han subido imagenes'});
+		}
+}
+
+
+function removeFilesOfUploads(res, file_path, message){
+	fs.unlink(file_path, (err) =>{
+		return res.status(200).send({message: message});
+	});
+}
+
+
+
 module.exports = {
 	home,
 	pruebas,
@@ -195,6 +245,7 @@ module.exports = {
 	loginUser,
 	getUser,
 	getUsers,
-	updateUser
+	updateUser,
+	uploadImage
 
 }

76b9bfd0d7af98f9c2cebc41b15d38a614f70d85
john <maquistemas@gmail.com>
2018-06-13 17:03:03 -0500

    Seguir a usuario-followed

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -6,10 +6,22 @@ var mongoosePaginate = require('mongoose-pagination');
 var User = require('../models/user');
 var Follow = require('../models/follow');
 
-function prueba(req, res){
-	res.status(200).send({message: 'Hola mundo desde el controlador follows'});
+function saveFollow(req, res){
+	var params = req.body;
+	var follow = new Follow();
+	follow.user = req.user.sub;
+	follow.followed = params.followed;
+
+	follow.save((err, followStored) => {
+		if(err) return res.status(500).send({message: 'Error al guardar el seguimiento'});
+		
+		if(!followStored) return res.status(404).send({message:'El seguimiento no se ha guardado'});
+
+		return res.status(200).send({follow:followStored});
+	}); 
+
 }
 
 module.exports = {
-	prueba
+	saveFollow
 }
--- api/routes/follow.js
+++ api/routes/follow.js
@@ -5,6 +5,6 @@ var FollowController = require('../controllers/follow');
 var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
-api.get('/pruebas-follow', md_auth.ensureAuth, FollowController.prueba);
+api.post('/follow', md_auth.ensureAuth, FollowController.saveFollow);
 
 module.exports = api; 

ee8d9da587fb6325d6618a7d23d58edbcb85b80e
john <maquistemas@gmail.com>
2018-06-13 12:26:23 -0500

    Devolver imagenes de usuario

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -246,6 +246,20 @@ function removeFilesOfUploads(res, file_path, message){
 
 
 
+function getImageFile(req, res){
+	var image_file = req.params.imageFile;
+	var path_file = './uploads/users/'+image_file;
+
+	fs.exists(path_file, (exists) => {
+		if(exists){
+			res.sendFile(path.resolve(path_file));
+		}else{
+			res.status(200).send({message: 'No existe la imagen...'});
+		}
+	});
+}
+
+
 module.exports = {
 	home,
 	pruebas,
@@ -254,6 +268,7 @@ module.exports = {
 	getUser,
 	getUsers,
 	updateUser,
-	uploadImage
+	uploadImage,
+	getImageFile
 
 }

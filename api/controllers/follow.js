295383192927af7ca7d969f37f4b71f3c9ed3993
john <maquistemas@gmail.com>
2018-06-14 12:50:15 -0500

    Listado de seguidores

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -66,8 +66,42 @@ function getFollowingUsers(req, res){
 }
 
 
+function getFollowedUsers(req, res){
+	var userId = req.user.sub;
+
+	if(req.params.id && req.params.page){
+		userId = req.params.id;
+	}
+
+	var page = 1;
+
+	if(req.params.page){
+		page = req.params.page;
+	}else{
+		page = req.params.id;
+	}
+
+	var itemsPerPage = 4;
+
+	Follow.find({followed:userId}).populate('user').paginate(page, itemsPerPage,(err, follows, total) =>{
+		if(err) return res.status(500).send({message: 'Error en el servidor'});
+
+		if(!follows) return res.status(404).send({message: 'No te sigue ningun usario'});
+		
+		return res.status(200).send({
+			total: total,
+			pages: Math.ceil(total/itemsPerPage),
+			follows
+		});
+	});
+
+
+}
+
+
 module.exports = {
 	saveFollow,
 	deleteFollow,
-	getFollowingUsers
+	getFollowingUsers,
+	getFollowedUsers
 }

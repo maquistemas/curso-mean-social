35c4cb88d42e608ed8084ae83e50e054f2dcd522
john <maquistemas@gmail.com>
2018-06-14 13:53:25 -0500

    Listado de usuarios sin paginar

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -99,9 +99,34 @@ function getFollowedUsers(req, res){
 }
 
 
+//Devolver listado de usuarios 
+function getMyFollows(req,res){
+	var userId = req.user.sub;
+
+	var find = Follow.find({user:userId});
+
+	if(req.params.followed){
+		find = Follow.find({followed:userId});
+	}
+
+	find.populate('user followed').exec((err, follows) => {
+		if(err) return res.status(500).send({message: 'Error en el servidor'});
+
+		if(!follows) return res.status(404).send({message: 'No sigues a ningun usario'});
+		
+		return res.status(200).send({follows});
+
+	});
+
+}
+
+
+
+
 module.exports = {
 	saveFollow,
 	deleteFollow,
 	getFollowingUsers,
-	getFollowedUsers
+	getFollowedUsers,
+	getMyFollows
 }
--- api/routes/follow.js
+++ api/routes/follow.js
@@ -9,6 +9,8 @@ api.post('/follow', md_auth.ensureAuth, FollowController.saveFollow);
 api.delete('/follow/:id', md_auth.ensureAuth, FollowController.deleteFollow);
 api.get('/following/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowingUsers);//id?y page? ,son opcionales
 api.get('/followed/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowedUsers);
+api.get('/get-my-follows/:followed?', md_auth.ensureAuth, FollowController.getMyFollows);
+
 
 
 module.exports = api; 

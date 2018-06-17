e417fc95eb1977217dff27ffcee47c2fd5eb581e
john <maquistemas@gmail.com>
2018-06-17 12:40:47 -0500

    Guardar nuevas publicaciones

--- api/controllers/publication.js
+++ api/controllers/publication.js
@@ -17,6 +17,28 @@ function probando(req, res){
 }
 
 
+function savePublication(req, res){
+	var params = req.body;
+
+	if(!params.text) return res.status(200).send({message: ' Debes enviar un texto!'});
+
+	var publication = new Publication();
+	publication.text = params.text;
+	publication.file = 'null';
+	publication.user = req.user.sub;
+	publication.created_at = moment().unix();
+
+	publication.save((err, publicationStored) => {
+		if(err) return res.status(500).send({message: 'Error al guardar la publicacion'});
+
+		if(!publicationStored ) return res.status(404).send({message: 'La publicación no ha sido guardada'});
+
+		return res.status(200).send({publication: publicationStored});
+	});
+}
+
+
 module.exports = {
-	probando
+	probando,
+	savePublication
 }

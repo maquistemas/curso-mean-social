770615a05c4952b03f91e7106a6849e90f45d90d
john <maquistemas@gmail.com>
2018-06-17 12:18:12 -0500

    Controlador y rutas para publicaciones

--- api/app.js
+++ api/app.js
@@ -8,6 +8,7 @@ var app = express();
 //cargar rutas
 var user_routes = require('./routes/user');
 var follow_routes = require('./routes/follow');
+var publication_routes = require('./routes/publication');
 
 //middlewares (son métodos que se ejecutan antes que lleguen a un controlador
 //en cada petición se va ejecutar el middlewares
@@ -20,6 +21,7 @@ app.use(bodyParser.json());
 //rutas
 app.use('/api', user_routes);
 app.use('/api', follow_routes);
+app.use('/api', publication_routes);
 
 
 //exportar

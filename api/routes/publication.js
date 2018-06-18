d01402c4646f21211bad81d1082004b1a2fb016a
john <maquistemas@gmail.com>
2018-06-18 05:52:48 -0500

    Publicaciones timeline

--- api/routes/publication.js
+++ api/routes/publication.js
@@ -10,6 +10,7 @@ var md_upload = multipart({ uploadDir: './uploads/publications'});
 
 api.get('/probando-pub', md_auth.ensureAuth, PublicationController.probando);
 api.post('/publication', md_auth.ensureAuth, PublicationController.savePublication);
+api.get('/publications/:page?', md_auth.ensureAuth, PublicationController.getPublications);
 
 
 module.exports = api;

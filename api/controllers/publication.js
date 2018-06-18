a72e03e13a2388703216e94753f29fdd27e704d1
john <maquistemas@gmail.com>
2018-06-18 07:21:20 -0500

    Elimanr publicacion

--- api/controllers/publication.js
+++ api/controllers/publication.js
@@ -90,9 +90,36 @@ function getPublication(req, res){
 	});
 }
 
+
+/*
+function deletePublication(req, res){
+	var publicationId = req.params.id;
+
+	Publication.find({'user': req.user.sub, '_id': publicationId}).remove((err, publicationRemoved) => {
+		if(err) return res.status(500).send({message: 'Error al borrar publicaciones'});
+		
+		if(!publicationRemoved) return res.status(404).send({message: 'No se ha borrado la publicación'});
+	
+		return res.status(200).send({publication: publicationRemoved});
+	});
+}
+*/
+
+function deletePublication(req, res){
+	var publicationId = req.params.id;
+
+	Publication.find({'user': req.user.sub, '_id': publicationId}).remove(err => {
+		if(err) return res.status(500).send({message: 'Error al borrar publicaciones'});
+		
+		return res.status(200).send({message: 'Publicacion eleimnada correctamente'});
+	});
+}
+
+
 module.exports = {
 	probando,
 	savePublication,
 	getPublications,
-	getPublication
+	getPublication,
+	deletePublication
 }

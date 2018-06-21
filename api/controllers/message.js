4e4d1554769f300d1bddbf5b3f39e5931c871424
john <maquistemas@gmail.com>
2018-06-21 18:31:56 -0500

    Mensajes recibidos

--- api/controllers/message.js
+++ api/controllers/message.js
@@ -36,7 +36,34 @@ function saveMessage(req, res){
 	
 }
 
+
+function getReceivedMessages(req, res){
+	var userId = req.user.sub;
+
+	var page = 1;
+	if(req.params.page){
+		page = req.params.page;
+	}
+
+	var itemsPerPage = 4;
+
+	Message.find({receiver: userId}).populate('emitter').paginate(page, itemsPerPage, (err, messages, total) => {
+		if(err) return res.status(500).send({message: 'Error en la petición'});
+		if(!messages) return res.status(404).send({message: 'No hay mensajes'});
+		
+		return res.status(200).send({
+			total: total,
+			pages: Math.ceil(total/itemsPerPage),
+			messages
+		});
+	});
+
+}
+
+
+
 module.exports = {
 	probando,
-	saveMessage
+	saveMessage,
+	getReceivedMessages
 }
--- api/routes/message.js
+++ api/routes/message.js
@@ -8,5 +8,6 @@ var md_auth = require('../middlewares/authenticated');
 
 api.get('/probando-md', md_auth.ensureAuth, MessageController.probando);
 api.post('/message', md_auth.ensureAuth, MessageController.saveMessage);
+api.get('/my-messages/:page?', md_auth.ensureAuth, MessageController.getReceivedMessages);
 
 module.exports = api;

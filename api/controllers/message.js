c2d8c31a485c5d2a0d9d988aed71de06d48d72b2
john <maquistemas@gmail.com>
2018-07-06 15:26:18 -0500

    message

--- api/controllers/message.js
+++ api/controllers/message.js
@@ -48,7 +48,7 @@ function getReceivedMessages(req, res){
 
 	var itemsPerPage = 4;
 
-	Message.find({receiver: userId}).populate('emitter', 'name surname image nick _id').paginate(page, itemsPerPage, (err, messages, total) => {
+	Message.find({receiver: userId}).populate('emitter', 'name surname image nick _id').sort('-created_at').paginate(page, itemsPerPage, (err, messages, total) => {
 		if(err) return res.status(500).send({message: 'Error en la petición'});
 		if(!messages) return res.status(404).send({message: 'No hay mensajes'});
 		
@@ -73,7 +73,7 @@ function getEmmitMessages(req, res){
 
 	var itemsPerPage = 4;
 
-	Message.find({emitter: userId}).populate('emitter receiver', 'name surname image nick _id').paginate(page, itemsPerPage, (err, messages, total) => {
+	Message.find({emitter: userId}).populate('emitter receiver', 'name surname image nick _id').sort('-created_at').paginate(page, itemsPerPage, (err, messages, total) => {
 		if(err) return res.status(500).send({message: 'Error en la petición'});
 		if(!messages) return res.status(404).send({message: 'No hay mensajes'});
 		
